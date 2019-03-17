73aeda13dffd02c5e2873f463488e614a92a08c6
Mahbub <prof.bub@live.com>
2019-03-18 04:47:56 +0700

    change localhost to domain name

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,4 @@
-// const baseURL = "http://miniwp-server.cloudeyeglobal.com";
-const baseURL = "http://localhost:3000";
+const baseURL = "http://miniwp-server.cloudeyeglobal.com";
 
 var headers = {};
 
